0d3a34a2f174a495a0cff4d4cb306e2c0df4fe9d
Itswali <luckywali0986@gmail.com>
2026-03-04 22:12:34 +0500

    created simple navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,21 @@
+import { Link } from "react-scroll";
 
-
-export default function Navbar() {
+const Navbar = () => {
   return (
-    <div>
+    <nav style={{ position: 'fixed', top: 0, width: '100%', background: '#fff', zIndex: 1000 }}>
+      <ul style={{ display: 'flex', listStyle: 'none', gap: '20px', padding: '1rem' }}>
+        <li>
+          <Link to="home" spy={true} smooth={true} offset={-70} duration={500}>Home</Link>
+        </li>
+        <li>
+          <Link to="about" spy={true} smooth={true} offset={-70} duration={500}>About</Link>
+        </li>
+        <li>
+          <Link to="projects" spy={true} smooth={true} offset={-70} duration={500}>Projects</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-    </div>
-  )
-}
+export default Navbar;
