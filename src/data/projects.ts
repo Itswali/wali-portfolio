796ad6f09523aad6d232f8ff6dfc73d34542bdd1
Itswali <luckywali0986@gmail.com>
2026-03-09 21:47:21 +0500

    created project data files for project interface and its objects

---
+++ src/data/projects.ts
@@ -0,0 +1,74 @@
+export interface Project {
+  id: string;
+  image: string;
+  title: string;
+  infoProject: string[];
+  description: string;
+  technologies: string[];
+  linkLiveVersion: string;
+  linkSource: string;
+}
+
+export const projects: Project[] = [
+  {
+    id: '1',
+    image: 'https://images.unsplash.com/photo-1504384308090-c894fdcc538d?q=80&w=800',
+    title: 'Global-Summit',
+    infoProject: ['Global Summit', 'Full Stack Dev', '2022'],
+    description: 'This is a UN Refugee Advert page for an annual Summit meeting held in America. Featuring global speakers and key topics.',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    linkLiveVersion: 'https://itswali.github.io/Global-Summit/',
+    linkSource: 'https://github.com/Itswali/Global-Summit',
+  },
+  {
+    id: '2',
+    image: 'https://images.unsplash.com/photo-1484480974693-6ca0a78fb36b?q=80&w=800',
+    title: 'To-DO-List',
+    infoProject: ['CANOPY', 'Web Page', '2022'],
+    description: 'A simple web app managing a To-Do list. Users can add, edit, delete, and clear all items efficiently.',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    linkLiveVersion: 'https://itswali.github.io/toDoList/dist/',
+    linkSource: 'https://github.com/Itswali/toDoList',
+  },
+    {
+    id: '3',
+    image: 'img/mealsDB.png',
+    title: 'MealsDb',
+    infoProject: ['CANOPY', 'Full Stack Dev', '2022'],
+    description: 'This is a single page dynamic web app that show meals Description , Recipes and You can find your favorite meal by searching it , Its using API to load all the data and images.',
+    technologies: ['html', 'css', 'javaScript'],
+    linkLiveVersion: 'https://itswali.github.io/MealsDB/',
+    linkSource: 'https://github.com/Itswali/MealsDB/',
+  },
+  {
+    id: '4',
+    image: '../assets/BookStoore.png',
+    title: 'Book Store',
+    infoProject: ['Books', 'Full Stack Dev', '2022'],
+    description: 'This is a Bookstore web app created with React & Redux. A user can add a book with his author and category also can track its progress.',
+    technologies: ['html', 'scss', 'React', 'Redux'],
+    linkLiveVersion: 'https://book-store-qe1o.onrender.com/',
+    linkSource: 'https://github.com/Itswali/BookStore/',
+  },
+  {
+    id: '5',
+    image: 'img/SpaceStore.png',
+    title: 'Space Store',
+    infoProject: ['CANOPY', 'Front End Development', '2022'],
+    description: 'The Space Travelers Hub is a React App. It consists of Rockets, Missions, and the My Profile section. It uses Redux as a state management tool. You can join missions and reserve rockets and see them in Profile Section.',
+    technologies: ['html', 'Scss', 'React', 'Redex'],
+    linkLiveVersion: 'https://gpt-3-z9vl.onrender.com/',
+    linkSource: 'https://github.com/Itswali/Space-Traveler/',
+  },
+  {
+    id: '6',
+    image: 'img/GPT3.png',
+    title: 'GPT3',
+    infoProject: ['CANOPY', 'Front End Development', '2022'],
+    description: 'This is a single page dynamic web app that display the deatils of what GPT3 is and its information with appealing design.',
+    technologies: ['html', 'Scss', 'React'],
+    linkLiveVersion: 'https://gpt-3-z9vl.onrender.com/',
+    linkSource: 'https://github.com/Itswali/GPT3/',
+  },
+
+];
