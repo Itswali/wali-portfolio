a30e2ace21ef427c20ac035d4289c49aad02db4c
Itswali <luckywali0986@gmail.com>
2026-03-29 18:21:53 +0500

    fix about profile mobile stuck

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -16,10 +16,9 @@ const About: React.FC<AboutProps> = ({ data }) => {
   ];
 
   return (
-    <div className="h-screen w-full  flex flex-col lg:flex-row text-white overflow-hidden font-sans">
+   <div className="min-h-screen w-full flex flex-col lg:flex-row text-white font-sans overflow-x-hidden">
 
-      {/* LEFT SIDE: Fixed Sidebar (Based on Hamid's Design) */}
-      <aside className="w-full lg:w-120 p-8 lg:p-20 flex flex-col justify-between items-center lg:items-start border-r border-white/5 bg-black/30 backdrop-blur-sm">
+    <aside className="w-full lg:fixed lg:top-0 lg:left-0 lg:w-120 lg:h-screen p-8 lg:p-20 flex flex-col justify-between items-center lg:items-start border-r border-white/5 bg-black/30 backdrop-blur-sm">
         <div className="flex flex-col items-center lg:items-start space-y-10 w-full">
 
           {/* Avatar & Location */}
@@ -66,10 +65,10 @@ const About: React.FC<AboutProps> = ({ data }) => {
       </aside>
 
       {/* RIGHT SIDE: Content Area */}
-      <section className="flex-1 overflow-y-auto custom-scrollbar scroll-smooth ">
+      <section className="flex-1 overflow-y-auto custom-scrollbar scroll-smooth lg:ml-120">
         <div className="max-w-4xl mx-auto px-8 lg:px-24 py-20 lg:py-32 space-y-40">
 
-          {/* Introduction with Hamid's Header Style */}
+          {/* Introduction*/}
           <section id="introduction" className="space-y-8">
             <div className="inline-flex items-center gap-3 px-4 py-2 rounded-full border border-cyan-500/30 bg-cyan-500/5 text-cyan-400 text-xs font-bold hover:bg-cyan-500/10 transition-colors cursor-pointer group">
               <a href="https://drive.google.com/file/d/1EtOtTXFtqn7Vo_HGkYDhZmnW-mJbEUCk/view" target='blank'>Check my resume</a> <ArrowRight size={14} className="group-hover:translate-x-1 transition-transform" />
